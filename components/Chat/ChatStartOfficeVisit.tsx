40bb052dd9ee394f59ba9ed86babe0a3be01c6ea
Amigo100 <61164710+Amigo100@users.noreply.github.com>
2025-04-14 19:43:42 +0100

    Update ChatStartOfficeVisit.tsx

--- components/Chat/ChatStartOfficeVisit.tsx
+++ components/Chat/ChatStartOfficeVisit.tsx
@@ -106,9 +106,9 @@ export const ChatStartOfficeVisit = ({ onSend }: Props) => {
       className={`
         flex items-center justify-center w-64 h-24 
         rounded-lg border border-neutral-200 
-        bg-[#008080] bg-opacity-100
+        bg-gray-300 bg-opacity-100
         cursor-pointer 
-        hover:bg-[#008080] hover:bg-opacity-70
+        hover:bg-gray-300 hover:bg-opacity-70
         record-button 
         ${recording ? 'is-recording' : ''}
       `}
