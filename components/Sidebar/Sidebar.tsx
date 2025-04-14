f1945941e1c25b8cc5264d3279da5e7a80a74ffa
Amigo100 <61164710+Amigo100@users.noreply.github.com>
2025-04-14 22:32:48 +0100

    Update Sidebar.tsx

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -99,7 +99,7 @@ const Sidebar = <T,>({
               {handleCreateFolder && (
                 <button
                   className={`
-                    ml-2 flex flex-shrink-0 cursor-pointer items-center gap-3
+                    ml-3 flex flex-shrink-0 cursor-pointer items-center gap-3
                     rounded-md border border-gray-200 p-3 text-sm text-black
                     transition-colors duration-200 hover:bg-gray-100
                   `}
