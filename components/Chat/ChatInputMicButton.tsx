5ddaeb2dda4c516a1528007883ef56a785ffb7b6
Amigo100 <61164710+Amigo100@users.noreply.github.com>
2025-04-15 18:35:34 +0100

    Update ChatInputMicButton.tsx

--- components/Chat/ChatInputMicButton.tsx
+++ components/Chat/ChatInputMicButton.tsx
@@ -184,7 +184,7 @@ export const ChatInputMicButton = ({ onSend, messageIsStreaming }: Props) => {
       onClick={recording ? handleStopRecording : handleStartRecording}
       disabled={messageIsStreaming}
     >
-      <IconMicrophone size={18} />
+      <IconMicrophone size={24} />
     </button>
   );
 };
