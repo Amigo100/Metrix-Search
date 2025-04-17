6aa1f63b0ad4a6bb8c8ce937ea3bf28e13c47dd2
Amigo100 <61164710+Amigo100@users.noreply.github.com>
2025-04-17 15:47:21 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,23 @@
-import { DocumentProps, Head, Html, Main, NextScript } from 'next/document';
-
-import i18nextConfig from '../next-i18next.config';
-
-type Props = DocumentProps & {
-  // add custom document props
-};
+// file: pages/_document.tsx  (unchanged imports)
 
 export default function Document(props: Props) {
   const currentLocale =
     props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
+
   return (
     <Html lang={currentLocale}>
       <Head>
+        {/* PWA meta */}
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="mobile-web-app-title" content="Metrix"></meta>
+        <meta name="mobile-web-app-title" content="Metrix" />
+
+        {/* NEW: favicon & Apple touch icon */}
+        <link rel="icon" href="/favicon-v2.png" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          href="/favicon-v2.png"
+          sizes="180x180"
+        />
       </Head>
       <body>
         <Main />
