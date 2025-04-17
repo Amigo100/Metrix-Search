18ec4ca9d31eb78c9a6eb0cd47f6945ab626f6b2
Amigo100 <61164710+Amigo100@users.noreply.github.com>
2025-04-17 11:45:49 +0100

    Update OpenCloseButton.tsx

--- components/Sidebar/components/OpenCloseButton.tsx
+++ components/Sidebar/components/OpenCloseButton.tsx
@@ -12,7 +12,7 @@ export const CloseSidebarButton = ({ onClick, side }: Props) => {
       <button
         className={`fixed top-2.5 ${
           side === 'right' ? 'right-[320px]' : 'left-[320px]'
-        } z-50 h-7 w-7 hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
+        } z-50 h-7 w-7 hover:text-teal-700 dark:text-white dark:hover:text-teal-700 sm:top-0.5 sm:${
           side === 'right' ? 'right-[320px]' : 'left-[320px]'
         } sm:h-8 sm:w-8 sm:text-neutral-700`}
         onClick={onClick}
@@ -32,9 +32,9 @@ export const OpenSidebarButton = ({ onClick, side }: Props) => {
     <button
       className={`fixed top-2.5 ${
         side === 'right' ? 'right-2' : 'left-2'
-      } z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
+      } z-50 h-7 w-7 text-black hover:text-teal-700 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
         side === 'right' ? 'right-2' : 'left-2'
-      } sm:h-8 sm:w-8 sm:text-neutral-700`}
+      } sm:h-8 sm:w-8 sm:text-black`}
       onClick={onClick}
     >
       {side === 'right' ? <IconArrowBarLeft /> : <IconArrowBarRight />}
