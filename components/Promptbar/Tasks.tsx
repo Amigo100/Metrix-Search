4c5bdbc7198b5c4f1cf6b8e7cb8a8806e51da1af
Amigo100 <61164710+Amigo100@users.noreply.github.com>
2025-04-20 00:35:01 +1200

    Update Tasks.tsx
    Change to absolute path due to 'Dialog' export error.

--- components/Promptbar/Tasks.tsx
+++ components/Promptbar/Tasks.tsx
@@ -37,7 +37,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label'; // Needed by AddPatientModal
 import {
   Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle
-} from '@/components/ui/Dialog'; // Needed by AddPatientModal
+} from '../ui/Dialog'; // Needed by AddPatientModal (Using relative path)
 
 // --- Import Moved Core Component ---
 import { PatientCard } from '@/components/patients/PatientCard'; // <== UPDATED PATH
